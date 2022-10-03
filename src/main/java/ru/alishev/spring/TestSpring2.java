18902c7c1ebb6185a772c31dbd37c54e91a7049c
Kim1350 <rholmogorov12@gmail.com>
2022-10-04 03:37:08 +0900

    lesson 7

--- src/main/java/ru/alishev/spring/TestSpring2.java
+++ src/main/java/ru/alishev/spring/TestSpring2.java
@@ -11,10 +11,20 @@ public class TestSpring2 {
         //Music music =  context.getBean("musicBean", Music.class);
 
         //MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusicList();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean comp = firstMusicPlayer == secondMusicPlayer;
+
+        System.out.println(comp);
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(10);
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+       // System.out.println(musicPlayer.getName());
+       // System.out.println(musicPlayer.getVolume());
         context.close();
     }
 }
