6db0d28ca2c53f7ccaaf4a08343d6561cdd84ce6
Kim1350 <rholmogorov12@gmail.com>
2022-10-04 02:57:54 +0900

    lesson 4

---
+++ src/main/java/ru/alishev/spring/Music.java
@@ -0,0 +1,6 @@
+package ru.alishev.spring;
+
+public interface Music {
+    String getSong();
+
+}
---
+++ src/main/java/ru/alishev/spring/TestSpring2.java
@@ -0,0 +1,18 @@
+package ru.alishev.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring2 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        Music music =  context.getBean("musicBean", Music.class);
+
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+        musicPlayer.playMusic();
+
+        context.close();
+    }
+}
