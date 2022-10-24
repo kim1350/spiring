7bba7f88654222c662133021654adb9700609a54
Kim1350 <rholmogorov12@gmail.com>
2022-10-25 01:59:38 +0900

    lesson10

--- src/main/java/ru/alishev/spring/MusicPlayer.java
+++ src/main/java/ru/alishev/spring/MusicPlayer.java
@@ -1,17 +1,25 @@
 package ru.alishev.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class MusicPlayer {
-    private Music music;
+    private ClassicalMusic classicalMusic;
+    private RockMusic rockMusic;
 
-    // IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
     }
 
-    public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+    public String playMusic() {
+        return "Playing: " + rockMusic.getSong();
+        /*System.out.println("Playing: " + classicalMusic.getSong());
+        System.out.println("Playing: " + rockMusic.getSong());*/
     }
 }
--- src/main/java/ru/alishev/spring/TestSpring2.java
+++ src/main/java/ru/alishev/spring/TestSpring2.java
@@ -7,13 +7,18 @@ public class TestSpring2 {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
-        Music music = context.getBean("rockMusic", Music.class);
+       /* Music music = context.getBean("rockMusic", Music.class);
         MusicPlayer musicPlayer = new MusicPlayer(music);
         musicPlayer.playMusic();
 
         Music music2 = context.getBean("classicalMusic", Music.class);
         MusicPlayer classPlayer = new MusicPlayer(music2);
-        classPlayer.playMusic();
+        classPlayer.playMusic();*/
+
+        /*MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();*/
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
         context.close();
     }
 }
