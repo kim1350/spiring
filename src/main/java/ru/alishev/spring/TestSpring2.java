43553f59f6e9333812a0b0f680865bcb3f97757a
Kim1350 <rholmogorov12@gmail.com>
2022-10-31 18:34:26 +0900

    lesson 13

--- src/main/java/ru/alishev/spring/TestSpring2.java
+++ src/main/java/ru/alishev/spring/TestSpring2.java
@@ -1,13 +1,13 @@
 package ru.alishev.spring;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring2 {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                SpringConfig.class
         );
-
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         System.out.println(musicPlayer.getName());
         System.out.println(musicPlayer.getVolume());
